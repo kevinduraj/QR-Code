9d86ce845b0f9f059ac1dc0306e062e3002cd954
Kevin Duraj <kevinduraj@gmail.com>
2014-05-03 11:31:25 -0700

    compute 3% percentile from middle of otsu image

--- FinalProject/src/finalproject/Main.java
+++ FinalProject/src/finalproject/Main.java
@@ -8,30 +8,37 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-/*-------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------
+                            QR Code Detection
+/-------------------------------------------------------------------------------------------*/
 public class Main {
 
-    private static String filename;
     
     /*---------------------------------------------------------------------------------------*/
     public static void main(String[] args) throws IOException {
 
-        DetectQRCode("qrcode3");
+        String output = DetectQRCode("qrcode2");
+        System.out.println("6. output = " + output);
+        
+        int[][] out = ImageRead(output);
+        Probability(out);
         
     }
     /*---------------------------------------------------------------------------------------*/
-    private static void DetectQRCode(String filename) throws IOException {
+    private static String DetectQRCode(String filename) throws IOException {
         
         ProcessHough(filename);
         
         /*-------------- Opening -----------*/
-        BufferedImage image1 = ReadBI2Gray("src/output1/"+filename+"a-hough.png");                
+        System.out.println("1. Opening");
+        BufferedImage image1 = ReadBI2Gray("src/output1/"+filename+"a-hough.png");   
         AbstractOperation opening1 = new Opening();
         BufferedImage bi1 = opening1.execute(image1);
         File output1 = new File("src/output1/"+filename+"b-opening.png");
         ImageIO.write(bi1, "png", output1);
         
         /*-------------- Erode -------------*/
+        System.out.println("2. Errode");        
         BufferedImage image3 = ReadBI2Gray("src/output1/"+filename+"b-opening.png");                
         AbstractOperation erode3 = new Erosion();
         BufferedImage bi3 = erode3.execute(image3);
@@ -43,13 +50,16 @@ public class Main {
         //String filename = otsu.run("src/output/OtsuBin");
        
         /*------- Sobel Edge Detection ------*/
+        System.out.println("3. Sobel edge detection - Magnitude");        
         Sobel sobel = new Sobel();
         sobel.process("src/output1/"+filename+"c-erode.png");
-        ImageWrite(sobel.Magnitute, "src/output1/"+filename+"e-magnitude.png");    
+        ImageWrite(sobel.Magnitute, "src/output1/"+filename+"e-sobel-mag.png");    
         
         /*------------------ Otsu Binarize ----------------------*/
-        OtsuBinarize otsu = new OtsuBinarize("src/output1/"+filename+"e-magnitude.png");
-        otsu.run("src/output2/"+filename+"-otsu");
+        System.out.println("4. Otsu binarize");        
+        OtsuBinarize otsu = new OtsuBinarize("src/output1/"+filename+"e-sobel-mag.png");
+        String output = otsu.run("src/output2/"+filename+"-otsu");
+        return output;
     }
 
     /*---------------------------------------------------------------------------------------*/
@@ -98,7 +108,53 @@ public class Main {
             File outputfile = new File(filename);
             ImageIO.write(bi, "png", outputfile);
     }
-    /*---------------------------------------------------------------------------------------*/     
-    
+    /*---------------------------------------------------------------------------------------*/
+
+    private static int[][] ImageRead(String filename) {
+
+        try {
+            File infile = new File(filename);
+            BufferedImage bi = ImageIO.read(infile);
+
+            int red[][] = new int[bi.getHeight()][bi.getWidth()];
+            int grn[][] = new int[bi.getHeight()][bi.getWidth()];
+            int blu[][] = new int[bi.getHeight()][bi.getWidth()];
+            for (int i = 0; i < red.length; ++i) {
+                for (int j = 0; j < red[i].length; ++j) {
+                    red[i][j] = bi.getRGB(j, i) >> 16 & 0xFF;
+                    grn[i][j] = bi.getRGB(j, i) >> 8 & 0xFF;
+                    blu[i][j] = bi.getRGB(j, i) & 0xFF;
+                }   
+            }   
+
+            return grn;
+
+        } catch (IOException e) {
+            System.out.println("image I/O error");
+            return null;
+        }   
+    }   
+    /*----------------------------------------------------------------------------------------*/    
+    private static void Probability(int[][] image) {
+        int h = image.length;
+        int w = image[0].length;
+        int half = (int) h /2;
+        
+        int range1 = (int) (half-(half*0.03));
+        int range2 = (int) (half+(half*0.03)+2);
+        
+        
+        for (int height = range1; height < range2; height++) {
+            System.out.format("%3d ", height);
+            
+            for (int width = 0; width < w; width++) {
+
+                System.out.format("%3d ", image[height][width]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    } 
+    /*----------------------------------------------------------------------------------------*/
     
 }
